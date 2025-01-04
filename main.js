1143db1d18c9a14297bacafa0239a53a066916e1
akshaya-24-tech <129148020+akshaya-24-tech@users.noreply.github.com>
2025-01-04 12:58:19 +0530

    Javascript Source Code for Portfolio.

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ let navLinks = document.querySelectorAll('header nav a');
 window.onscroll = () => {
 	sections.forEach(sec => {
 		let top = window.scrollY;
-		let offset = sec.offsetTop - 150;
+		let offset = sec.offsetTop-150;
 		let height = sec.offsetHeight;
 		let id = sec.getAtrribute('id');
 		
